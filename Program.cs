6bdf893fb9d6e5fec04667d46fa10e1c5af1eae1
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-14 14:48:52 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConDay5Assign6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] studentScores = new int[3][];
+            studentScores[0] = new int[] { 85, 92, 78 };
+            studentScores[1] = new int[] { 90, 87, 93, 89 };
+            studentScores[2] = new int[] { 76, 88 };
+
+            // Print the scores of each student
+            Console.WriteLine("Student Scores:");
+            for (int i = 0; i < studentScores.Length; i++)
+            {
+                Console.Write("Student " + (i + 1) + ": ");
+                for (int j = 0; j < studentScores[i].Length; j++)
+                {
+                    Console.Write(studentScores[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Calculate and print the average score for each student
+            Console.WriteLine("\nAverage Scores:");
+            for (int i = 0; i < studentScores.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < studentScores[i].Length; j++)
+                {
+                    sum += studentScores[i][j];
+                }
+                double average = (double)sum / studentScores[i].Length;
+                Console.WriteLine("Student " + (i + 1) + ": " + average);
+            }
+
+            // Calculate and print the average score for all students combined
+            int totalSum = 0;
+            int totalCount = 0;
+            for (int i = 0; i < studentScores.Length; i++)
+            {
+                for (int j = 0; j < studentScores[i].Length; j++)
+                {
+                    totalSum += studentScores[i][j];
+                    totalCount++;
+                }
+            }
+            double totalAverage = (double)totalSum / totalCount;
+            Console.WriteLine("\nAverage Score for all Students: " + totalAverage);
+
+            Console.ReadLine();
+        }
+    }
+}
